c9e12bb6e10892dc6d4d2855d8f3e988d10f5402
destifo <estifanosbireda@gmail.com>
2024-01-24 20:46:53 +0300

    fix the state not being provided issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './app.router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./core/state/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppRouter />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-    
   </React.StrictMode>
 );
 
